ffbf37ab8d5243772aaf65cbb33bc20907dd594a
Erin L. Kolp <erinlkolpfoss@gmail.com>
2025-02-10 11:24:59 -0700

    Adds json pretty.

--- lambda/app.py
+++ lambda/app.py
@@ -2,6 +2,7 @@ from aws_lambda_powertools import Logger, Tracer, Metrics
 from aws_lambda_powertools.event_handler.api_gateway import ApiGatewayResolver, ProxyEventType
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.metrics import MetricUnit
+import json
 
 logger = Logger()
 tracer = Tracer()
@@ -13,7 +14,8 @@ app = ApiGatewayResolver(proxy_type=ProxyEventType.APIGatewayProxyEventV2)
 def hello():
     metrics.add_metric(name="HelloWorldInvocations", unit=MetricUnit.Count, value=1)
     logger.info("Hello world endpoint called")
-    return {"message": "Hello World!"}
+    response = {"message": "Hello World!"}
+    return json.dumps(response, indent=4)
 
 @app.get("/hello/<name>")
 @tracer.capture_method
